f3a641dd0a997a105c111a9340806dba67744cc0
Stephen Chen <chen.stephen.r@gmail.com>
2018-05-16 09:14:15 -0700

    updated cell

--- Cell.java
+++ Cell.java
@@ -1,65 +1,30 @@
 package gameoflifepackage;
 
 public class Cell {
+	private boolean isLiving;
+	private boolean willLive;
 
-	private int row;
-	private int col;
-	private boolean isliving;
-	private boolean nextliving;
-
-
-	public Cell(int i, int j,boolean isliving) {
-		row=i;
-		col=j;
-		this.isliving=isliving;
+	public Cell(boolean isLiving) {
+		this.isLiving = isLiving;
 	}
 
-	//getters
-
-	public int getrow()
-	{
+	public int getRow() {
 		return row;
 	}
-	public int getcol()
-	{
+
+	public int getCol() {
 		return col;
 	}
-	public boolean getisliving()
-	{
-		return isliving;
+
+	public boolean isLiving() {
+		return isLiving;
 	}
 
-	public boolean getnextliving()
-	{
+	public boolean getnextliving() {
 		return nextliving;
 	}
 
-
-	public void cellclicked(int i,int j)
-	{
-		if (isliving==true)
-		{
-			isliving=false;
-		}
-		else
-		{
-			isliving=true;
-		}
+	public void swapIsLivingState() {
+		isLiving = !isLiving;
 	}
-	public boolean getMatrix(Cell cell) {
-		int[][] matrix = new int[cell.getrow()][cell.getcol()];
-
-		for(int i = 0, i > cell.getrow(), i++){
-			for(int j = 0, j > cell.getcol(), j++){
-				if (cell(i,j).getisliving == true){
-					maxtrix[i][j] = 1;
-				}else if((cell(i,j).getisliving == false){
-					maxtrix[i][j] = 0;
-				}
-			}
-		}
-		return matrix;
-	}
-
-
 }
--- Mananger.java
+++ Mananger.java
@@ -2,73 +2,73 @@ package gameoflifepackage;
 java.lang.Math.abs()
 public class Mananger {
 
-    Integer[][] matrix = new Integer[rowLen][colLen];
+  Integer[][] matrix = new Integer[rowLen][colLen];
 
-    class Mananger(Integer[][] matrix) {
-        this.matrix = matrix;
-    }
-// live (1) or dead (0)
-    public boolean nextStep(Integer[][] matrix) {
-        int rowLen = matrix.length;
-        int colLen = matrix[0].length;
+  class Mananger(Integer[][] matrix) {
+    this.matrix = matrix;
+  }
+  // live (1) or dead (0)
+  public boolean nextStep(Integer[][] matrix) {
+    int rowLen = matrix.length;
+    int colLen = matrix[0].length;
 
-        for(int i = 0; i < rowLen; i++) {
-            for (int j = 0; j < colLen; j++) {
-                boolean change = ifChange(matrix, i, j);
-                if (change == false) {
-                    continue;
-                } else {
-                    if (matrix[i][j] == 1) {
-                        matrix[i][j] = -1;
-                    } else if (matrix[i][j] == 0) {
-                        matrix[i][j] = -2;
-                    }
-                }
-            }
+    for(int i = 0; i < rowLen; i++) {
+      for (int j = 0; j < colLen; j++) {
+        boolean change = ifChange(matrix, i, j);
+        if (change == false) {
+          continue;
+        } else {
+          if (matrix[i][j] == 1) {
+            matrix[i][j] = -1;
+          } else if (matrix[i][j] == 0) {
+            matrix[i][j] = -2;
+          }
         }
-
-        for(int i = 0; i < rowLen; i++) {
-            for (int j = 0; j < colLen; j++) {
-                if (matrix[i][j] == -1) {
-                    matrix[i][j] = 0;
-                } else if (matrix[i][j] == -2) {
-                    matrix[i][j] = 1;
-                }
-            }
-        }
+      }
     }
 
-    public boolean ifChange(int[][] matrix, int i, int j) {
-        int num = neighbor(matrix, i, j);
-        if (matrix[i][j] == 1){
-            if (num < 2){
-                return true;
-            }else if(num == 2 || num == 3){
-                return false;
-            }else if(num > 3){
-                return true;
-            }
-        }else if(matrix[i][j] == 0 && num == 3){
-            return true;
+    for(int i = 0; i < rowLen; i++) {
+      for (int j = 0; j < colLen; j++) {
+        if (matrix[i][j] == -1) {
+          matrix[i][j] = 0;
+        } else if (matrix[i][j] == -2) {
+          matrix[i][j] = 1;
         }
+      }
     }
+  }
 
-    public int neighbor(int[][] matrix, int i, int j) {
-        int[] x = {1, -1, 0};
-        int[] y = {1, -1, 0};
-        int rowLen = matrix.length;
-        int colLen = matrix[0].length;
-        int sum = 0;
-        for (int x1 : x) {
-            for (int y1 : y) {
-                if (x1 == 0 && y1 == 0) {
-                    continue;
-                }
-                if (i + x1 >= 0 && i + x1 < rowLen && j + y1 >= 0 && j + y1 < colLen and abs
-                (matrix[i + x1][j + y1]) == 1){
-                    sum++;
-                }
-            }
-            return sum;
-        }
+  public boolean ifChange(int[][] matrix, int i, int j) {
+    int num = neighbor(matrix, i, j);
+    if (matrix[i][j] == 1){
+      if (num < 2){
+        return true;
+      }else if(num == 2 || num == 3){
+        return false;
+      }else if(num > 3){
+        return true;
+      }
+    }else if(matrix[i][j] == 0 && num == 3){
+      return true;
     }
+  }
+
+  public int neighbor(int[][] matrix, int i, int j) {
+    int[] x = {1, -1, 0};
+    int[] y = {1, -1, 0};
+    int rowLen = matrix.length;
+    int colLen = matrix[0].length;
+    int sum = 0;
+    for (int x1 : x) {
+      for (int y1 : y) {
+        if (x1 == 0 && y1 == 0) {
+          continue;
+        }
+        if (i + x1 >= 0 && i + x1 < rowLen && j + y1 >= 0 && j + y1 < colLen and abs
+        (matrix[i + x1][j + y1]) == 1){
+          sum++;
+        }
+      }
+      return sum;
+    }
+  }
